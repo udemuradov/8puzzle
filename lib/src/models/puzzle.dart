e9b3453977ae6eeedfc39d8b92823a0f0a3adf7b
Mac Os <macos@Macs-MBP.lan>
2022-08-17 12:45:21 +0500

    done

--- lib/src/models/puzzle.dart
+++
@@ -1,9 +0,0 @@
-import 'package:equatable/equatable.dart';
-
-class PuzzleModel extends Equatable {
-  final int value;
-  final String imagePath;
-  const PuzzleModel({required this.value, required this.imagePath});
-  @override
-  List<Object?> get props => [value, imagePath];
-}
--- lib/src/utils/constants.dart
+++
@@ -1,7 +0,0 @@
-class AppImages {
-  AppImages._();
-
-  static const String bg = 'assets/bg.jpg';
-  static const String original = 'assets/original.jpg';
-  static const String bird = 'assets/bird.png';
-}
